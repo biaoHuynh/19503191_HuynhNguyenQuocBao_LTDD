4cd9939fa084e028afaca019c64b7d45a2093357
baobao <hnquocbao22@gmail.com>
2022-02-22 08:32:36 +0700

    tuan2

---
+++ app/src/main/java/com/example/a19503191_huynhnguyenquocbao_ltdd/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.a19503191_huynhnguyenquocbao_ltdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
